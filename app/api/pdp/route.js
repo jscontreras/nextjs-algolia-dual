5c8cc48edbaa170e84867d652a20e07d5175b963
Sebastian Contreras <js.contreras@outlook.com>
2024-01-31 08:17:23 -0600

    Adding patch to see where is the error.

--- app/api/pdp/route.js
+++ app/api/pdp/route.js
@@ -10,8 +10,15 @@ export async function GET() {
  * Using uncached by default.
  */
 export async function POST(req) {
-  const data = await req.json();
-  // This is cached by objectId
-  const product = await getProductInfo(data.objectId);
-  return Response.json(product);
+  try {
+    const data = await req.json();
+    // This is cached by objectId
+    const product = await getProductInfo(data.objectId);
+    return Response.json(product);
+  } catch (err){
+    console.error(err);
+    const product = await getProductInfo('M0E20000000EAAK');
+    return Response.json({ message: 'Hello World!', product });
+  }
+
 }
